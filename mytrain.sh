46647fe3328484cd15289d0c5bea9b9d8288588d
Arvid-pku <xjyin2019@163.com>
2021-01-19 02:52:45 +0000

    train char

--- mytrain.sh
+++ mytrain.sh
@@ -3,13 +3,13 @@ tgt=tgt
 bedropout=0.5
 ARCH=transformer
 DATAPATH=bertnmt/data/char
-SAVEDIR=checkpoints
+SAVEDIR=checkpoints/char
 mkdir -p $SAVEDIR
 
-CUDA_VISIBLE_DEVICES=1 python bertnmt/train.py $DATAPATH \
+CUDA_VISIBLE_DEVICES=2 python bertnmt/train.py $DATAPATH \
 -a $ARCH --optimizer adam --lr 0.0005 -s $src -t $tgt --label-smoothing 0.1 \
---reset-dataloader --num-workers 0 --bert-model-name 'bert-base-chinese' \
---dropout 0.3 --max-tokens 4000 --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
+--reset-dataloader --num-workers 2 --bert-model-name 'bert-base-chinese' \
+--dropout 0.3 --max-tokens 8000 --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
 --criterion label_smoothed_cross_entropy --max-update 150000 --warmup-updates 4000 --warmup-init-lr '1e-07' \
 --adam-betas '(0.9,0.98)' --save-dir $SAVEDIR --share-all-embeddings \
 --encoder-bert-dropout --encoder-bert-dropout-ratio $bedropout | tee -a $SAVEDIR/training.log
